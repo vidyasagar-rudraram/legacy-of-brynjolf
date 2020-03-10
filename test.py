8b5f3a577af9eb06765b4fe4306ded8be75b8333
Vidyasagar Rudraram <vidyasagar@micropyramid.com>
2020-03-10 21:41:20 +0530

    test cases updated

--- test.py
+++ test.py
@@ -7,35 +7,42 @@ from util import path_dict, short_distance, find_character_index
 from test_base import set_keyboard_input, get_display_output
 
 
-r = Room('room.txt')
-(x, y) = r.brynjolf
-b = Brynjolf(r.room, r.brynjolf, r.guards, r.exit)
+def set_room():
+    r = Room('room.txt')
+    b = Brynjolf(r.room, r.brynjolf, r.guards, r.exit)
+    return r, b
 
 
-def test_short_distance():
-    assert short_distance(r.brynjolf, r.exit)[0] == 'r'
+def test_room():
+    r, b = set_room()
+    assert r.room == [[0, 0, 0, 'X'], ['G', 0, 0, 'X'], [0, 'B', 0, 'E'], ['X', 0, 'G', 0]]
 
 
-def find_bynjolf():
-    assert find_character_index(r.room, "B") == r.brynjolf
+@pytest.mark.parametrize(
+    'room, brynjolf_exit_guards', [
+        ('room.txt', [(2, 1), (2, 3), [(1, 0), (3, 2)]]),
+        ('room1.txt', [(2, 1), (1, 3), [(1, 0), (3, 2)]])
+    ]
+)
+def test_brynjolf_exit_guards(room, brynjolf_exit_guards):
+    r = Room(room)
+    assert r.brynjolf == brynjolf_exit_guards[0]
+    assert r.exit == brynjolf_exit_guards[1]
+    assert r.guards == brynjolf_exit_guards[2]
 
-def find_exit():
-    assert find_character_index(r.room, "E") == r.exit
 
-def find_guards():
-    assert find_character_index(r.room, "G") == r.guards
-
-
-# @pytest.mark.parametrize(
-#     'direction, res', [
-#         ('l', 0),
-#         ('r', 1),
-#         ('u', 1),
-#         ('d', 2),
-#     ]
-# )
-def test_enlightenment():
-    set_keyboard_input(['l'])
+@pytest.mark.parametrize(
+    'direction, res', [
+        ('', 'win: rr'),
+        ('l', 'win: rrr'),
+        ('r', 'win: r'),
+        ('u', 'win: dr'),
+        ('d', 'win: urr'),
+    ]
+)
+def test_enlightenment(direction, res):
+    set_keyboard_input([direction])
+    r, b = set_room()
     enlightenment(b)
-    output = get_display_output()
-    # assert output
+    output = get_display_output()[-1]
+    assert output == res
